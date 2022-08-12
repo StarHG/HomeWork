ef6593e0888cddc1b57b31977bcc11a85f9fe12c
Vadim <Starhousegames@gmail.com>
2022-08-12 23:03:59 +0300

    Исправил инициализацию массива

--- Assets/Scripts/HomeWork1/Hard/Task1HardSecretCode1.cs
+++ Assets/Scripts/HomeWork1/Hard/Task1HardSecretCode1.cs
@@ -8,9 +8,8 @@ public class Task1HardSecretCode1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Task1HardSecretCode task31 = new Task1HardSecretCode();
-        string words = task31.Decode("2World1Hello");
-        Debug.Log($"Tas1 Hard: {words}");
+        
+        Debug.Log("Tas1 Hard: " + Decode("2World1Hello3Unity"));
     }
 
     // Update is called once per frame
@@ -18,14 +17,18 @@ public class Task1HardSecretCode1 : MonoBehaviour
     {
 
     }
-}
-class Task1HardSecretCode
-{
     public string Decode(string secretCode)
     {
-        string[] words = new string[secretCode.Length];
+        int countOfNum = 0;
+        for (int i = 0; i < secretCode.Length; i++)
+        {
+            if (char.IsDigit(secretCode[i]))
+            {
+                countOfNum++;
+            }
+        }
+        string[] words = new string[countOfNum];
 
-        //string[] _words = _secretCode.Split(_separatingNumb, System.StringSplitOptions.None);
         for (int i = 0; i < secretCode.Length; i++)
         {
             if (char.IsDigit(secretCode[i]))
@@ -37,12 +40,14 @@ class Task1HardSecretCode
                     tmp += secretCode[j];
                     j++;
                 }
-                int tmp2 = int.Parse(Convert.ToString(secretCode[i]));
-                words[tmp2 - 1] = tmp;
+                int indexWordsTmp = int.Parse(Convert.ToString(secretCode[i]));
+                words[indexWordsTmp - 1] = tmp;
             }
         }
         secretCode = String.Join(" ", words);
         return secretCode;
     }
 }
+
+
 
