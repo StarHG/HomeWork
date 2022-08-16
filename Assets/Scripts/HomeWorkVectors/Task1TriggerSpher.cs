93c55600dace499af936e280ab16cea9109e6d29
Vadim <Starhousegames@gmail.com>
2022-08-16 22:55:44 +0300

    Первое задание по векторам

---
+++ Assets/Scripts/HomeWorkVectors/Task1TriggerSpher.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+ // 1.—фера с тригером при попадении в которую объекты окрашиваютс€ в красный. digstance
+ 
+ 
+
+public class Task1TriggerSpher : MonoBehaviour
+{
+    [SerializeField] private GameObject sphere2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, sphere2.transform.position);
+        var sphereRenderer = sphere2.GetComponent<Renderer>();
+        if (distance <= 1)                  
+            sphereRenderer.material.SetColor("_Color", Color.red);
+        else
+            sphereRenderer.material.SetColor("_Color", Color.white);
+
+    }
+
+    
+
+}
